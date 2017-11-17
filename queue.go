31fe6172ae37879fa9cb12951c5cd8de9e7d6061
Dinos Theodorou <dinos@crowdynews.com>
2017-11-17 09:39:51 +0100

    Add code documentation

--- queue.go
+++ queue.go
@@ -2,6 +2,7 @@ package queue
 
 import "container/list"
 
+// Queue is a queue structure implemented on top of a linked list
 type Queue struct {
 	storage *list.List
 }
@@ -11,19 +12,23 @@ func New() *Queue {
 	return new(Queue).Init()
 }
 
+// Init initializes our queue through its internal storage (linked list)
 func (q *Queue) Init() *Queue {
 	q.storage.Init()
 	return q
 }
 
+// Len returns the length of the queue
 func (q *Queue) Len() int {
 	return q.storage.Len()
 }
 
+// Enqueue adds a value in the end of the queue
 func (q *Queue) Enqueue(v interface{}) {
 	q.storage.PushBack(v)
 }
 
+// Dequeue removes and returns the first value of the queue
 func (q *Queue) Dequeue() interface{} {
 	el := q.storage.Front()
 	return q.storage.Remove(el)
